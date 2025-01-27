e7497c69c33df827dc0ea616c4ae3e1fda01f713
espadv69 <aitorjob69@gmail.com>
2025-01-27 12:33:32 +0100

    Add root to link html

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <StrictMode>
+    <h1>hello, world!</h1>
+  </StrictMode>
+)
