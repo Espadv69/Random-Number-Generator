d83dbc449124c716da54d204776174f051065764
espadv69 <aitorjob69@gmail.com>
2025-01-27 13:16:40 +0100

    Add App render

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import App from './App'
 import './css/global.css'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
   <StrictMode>
-    <h1>hello, world!</h1>
+    <App />
   </StrictMode>
 )
