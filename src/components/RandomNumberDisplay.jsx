e870ac421823b5ba14d395385eaad74e99767ea5
espadv69 <aitorjob69@gmail.com>
2025-01-27 13:21:12 +0100

    Add classname for h1, p-generatedNumber and counter

--- src/components/RandomNumberDisplay.jsx
+++ src/components/RandomNumberDisplay.jsx
@@ -1,12 +1,12 @@
 const RandomNumberDisplay = ({ randomNumber, count }) => {
   return (
     <div>
-      <h1>Random Number Generator</h1>
-      <p>
+      <h1 className="h1-title">Random Number Generator</h1>
+      <p className="generatedNumber">
         <strong>Generated Number:</strong>{' '}
         {randomNumber !== null ? randomNumber : 'N/A'}
       </p>
-      <p>
+      <p className="counter">
         <strong>Generation Count:</strong> {count}
       </p>
     </div>
