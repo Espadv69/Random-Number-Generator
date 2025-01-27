e598b62e4cc0cb1aeabc1f56abb1e0b2d684285a
espadv69 <aitorjob69@gmail.com>
2025-01-27 13:01:35 +0100

    Add utils import: { getRandomNumber }

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
 import RandomNumberDisplay from './components/RandomNumberDisplay'
 import GenerateButton from './components/GenerateButton'
 import RangeInput from './components/RangeInput'
+
+import { getRandomNumber } from './utils/randomUtils'
