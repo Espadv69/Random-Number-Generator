841f63d265b18bab290560e05746837b8ec008cc
espadv69 <aitorjob69@gmail.com>
2025-01-27 13:05:49 +0100

    Import useState and Add hooks: min, max, randomNumber and count

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { useState } from 'react'
+
 import RandomNumberDisplay from './components/RandomNumberDisplay'
 import GenerateButton from './components/GenerateButton'
 import RangeInput from './components/RangeInput'
 
 import { getRandomNumber } from './utils/randomUtils'
+
+const App = () => {
+  const [min, setMin] = useState(1) // Initial value
+  const [max, setMax] = useState(100) // Initial value
+  const [randomNumber, setRandomNumber] = useState(null) // Generated number
+  const [count, setCount] = useState(0) // Generation counter
+}
