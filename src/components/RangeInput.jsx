a798636b60e8cfa451891cf367c53a76fc60d8a6
espadv69 <aitorjob69@gmail.com>
2025-01-27 12:55:03 +0100

    Add handle input max's change

--- src/components/RangeInput.jsx
+++ src/components/RangeInput.jsx
@@ -2,4 +2,8 @@ const RangeInput = ({ min, max, setMin, setMax }) => {
   const handleMinChange = (e) => {
     setMin(Number(e.target.value))
   }
+
+  const handleMaxChange = (e) => {
+    setMax(Nummber(e.target.value))
+  }
 }
